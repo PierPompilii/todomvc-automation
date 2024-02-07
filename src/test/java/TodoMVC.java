28b1f04fafbdb66a85e09456c79cfc53552eb483
Pier Bruno Pompilii <pier340@gmail.com>
2024-02-07 16:26:29 +0000

    extra files with test

--- src/test/java/TodoMVC.java
+++ src/test/java/TodoMVC.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
 public class TodoMVC {
     private static ChromeDriver driver;
 
@@ -17,8 +19,38 @@ public class TodoMVC {
     // Your tests will go here!
     @Test
     void homepage() {
-        driver.get("https://todomvc.com/");
+        driver.get("https://todomvc.com/examples/angular/dist/browser/#/all");
     }
+    @Test
+    void addingItems() throws Exception {
+        driver.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+        WebElement inputBox = driver.findElement(By.cssSelector(".new-todo"));
+        inputBox.click();
+        inputBox.sendKeys("buy milk");
+        inputBox.sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+    }
+    @Test
+    void modifyItems() throws Exception {
+        driver.get("https://todomvc.com/examples/angular/dist/browser/#/all");
+        WebElement inputBox = driver.findElement(By.cssSelector(".new-todo"));
+        inputBox.click();
+        inputBox.sendKeys("buy milk");
+        inputBox.sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+        WebElement changeItem = driver.findElement(By.cssSelector(".view > label"));
+        Actions builder = new Actions(driver);
+        builder.doubleClick(changeItem).perform();
+        WebElement modifyItem = driver.findElement(By.id("edit-todo-input"));
+        modifyItem.sendKeys(Keys.valueOf("BACK_SPACE"));
+        modifyItem.sendKeys("buy bread");
+        modifyItem.sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+    }
+
+
+
+
 
     @AfterAll
     static void closeBrowser() {
