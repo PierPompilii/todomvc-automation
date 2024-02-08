c121f81cebd2b8d877ee652469cba5668a6c4f04
Muhtadi <muhtadimaahir@gmail.com>
2024-02-08 15:28:28 +0000

    Added Assertions to tests

--- src/test/java/TodoMVC.java
+++ src/test/java/TodoMVC.java
@@ -1,4 +1,5 @@
 
+import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -6,9 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import javax.swing.*;
 import java.util.concurrent.ExecutionException;
+
+
 
 public class TodoMVC {
     private static ChromeDriver driver;
@@ -32,7 +36,8 @@ public class TodoMVC {
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
         Thread.sleep(2000);
-        //assert list contains "buy milk"
+        WebElement firstItem = driver.findElement(By.cssSelector(".view > label"));
+        Assertions.assertEquals("buy milk", firstItem.getText());
     }
     @Test
     void modifyItems() throws Exception {
@@ -41,18 +46,16 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
-        WebElement changeItem = driver.findElement(By.cssSelector(".view > label"));
+        WebElement firstItem = driver.findElement(By.cssSelector(".view > label"));
         Actions builder = new Actions(driver);
-        builder.doubleClick(changeItem).perform();
+        builder.doubleClick(firstItem).perform();
         WebElement modifyItem = driver.findElement(By.id("edit-todo-input"));
         for (int i = 0; i < 100; i++) {
             modifyItem.sendKeys(Keys.BACK_SPACE);
         }
         modifyItem.sendKeys("buy bread");
         modifyItem.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
-        // assert list contains "buy bread"
+        Assertions.assertEquals("buy bread", firstItem.getText());
     }
 
     @Test
@@ -62,15 +65,12 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
-        WebElement changeItem = driver.findElement(By.cssSelector(".view > label"));
+        WebElement firstItem = driver.findElement(By.cssSelector(".view > label"));
         Actions builder = new Actions(driver);
-        builder.doubleClick(changeItem).perform();
-        Thread.sleep(2000);
-        WebElement page = driver.findElement(By.cssSelector("html"));
+        builder.doubleClick(firstItem).perform();
+        WebElement page = driver.findElement(By.cssSelector("body"));
         page.click();
-        Thread.sleep(2000);
-        // assert list contains "buy milk"
+        Assertions.assertEquals("buy milk", firstItem.getText());
     }
 
     @Test
@@ -80,11 +80,13 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
+        WebElement firstItem = driver.findElement(By.cssSelector(".view > label"));
         WebElement tickBox = driver.findElement(By.cssSelector(".toggle"));
         tickBox.click();
-        Thread.sleep(2000);
         // assert "buy milk" is in completed status
+        WebElement statusCompleted = driver.findElement(By.linkText("Completed"));
+        statusCompleted.click();
+        Assertions.assertTrue(firstItem.isDisplayed());
     }
 
     @Test
@@ -94,12 +96,13 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
+        WebElement firstItem = driver.findElement(By.cssSelector(".view > label"));
         WebElement tickBox = driver.findElement(By.cssSelector(".toggle"));
         tickBox.click();
         tickBox.click();
-        Thread.sleep(2000);
-        // assert "buy milk" is in active status
+        WebElement statusActive = driver.findElement(By.linkText("Active"));
+        statusActive.click();
+        Assertions.assertTrue(firstItem.isDisplayed());
     }
 
     @Test
@@ -109,14 +112,14 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
-        WebElement changeItem = driver.findElement(By.cssSelector(".view > label"));
+        WebElement firstItem = driver.findElement(By.cssSelector(".view > label"));
         Actions hover = new Actions(driver);
-        hover.moveToElement(changeItem).perform();
+        hover.moveToElement(firstItem).perform();
         WebElement buttonX = driver.findElement(By.cssSelector(".destroy"));
         buttonX.click();
-        Thread.sleep(2000);
         // assert list is empty
+        WebElement todoList = driver.findElement(By.tagName("app-todo-list"));
+        Assertions.assertEquals("", todoList.getText());
     }
 
     @Test
@@ -126,17 +129,19 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
+        WebElement itemCount = driver.findElement(By.className("todo-count"));
     //    assert "1 item is left"
+        Assertions.assertEquals("1 item left", itemCount.getText());
         WebElement tickBox = driver.findElement(By.cssSelector(".toggle"));
         tickBox.click();
     //  assert "0 items is left"
+        Assertions.assertEquals("0 items left", itemCount.getText());
         tickBox.click();
         inputBox.click();
         inputBox.sendKeys("buy bread");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
     //    assert "2 items are left"
+        Assertions.assertEquals("2 items left", itemCount.getText());
     }
 
     @Test
@@ -146,14 +151,14 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
         WebElement changeItem = driver.findElement(By.cssSelector(".view > label"));
         Actions hover = new Actions(driver);
         hover.moveToElement(changeItem).perform();
         WebElement buttonX = driver.findElement(By.cssSelector(".destroy"));
         buttonX.click();
-        Thread.sleep(2000);
-        // assert the status bar is hidden, if we can't find a solution, take screenshot
+        // assert the status bar is hidden, if we can't find a solution, take screenshot, assert false display
+        WebElement statusBar = driver.findElement(By.tagName("app-todo-footer"));
+        Assertions.assertFalse(statusBar.isDisplayed());
     }
 
     @Test
@@ -166,12 +171,24 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy bread");
         inputBox.sendKeys(Keys.ENTER);
-        WebElement firstTodo = driver.findElement(By.cssSelector("app-todo-item:nth-child(1) .toggle"));
-        firstTodo.click();
-        Thread.sleep(2000);
+        WebElement firstTodoToggle = driver.findElement(By.cssSelector("app-todo-item:nth-child(1) .toggle"));
+        firstTodoToggle.click();
+        WebElement firstTodo = driver.findElement(By.cssSelector("app-todo-item:nth-child(1) label"));
+        WebElement secondTodo = driver.findElement(By.cssSelector("app-todo-item:nth-child(2) label"));
         //assert buy milk and buy bread is in all status
+        WebElement statusAll = driver.findElement(By.linkText("All"));
+        statusAll.click();
+        Assertions.assertTrue(firstTodo.isDisplayed() && secondTodo.isDisplayed());
         //assert buy bread is in the active status
+        WebElement statusActive = driver.findElement(By.linkText("Active"));
+        statusActive.click();
+        Assertions.assertTrue(secondTodo.isDisplayed());
         //assert buy milk is in the completed status
+        WebElement statusCompleted = driver.findElement(By.linkText("Completed"));
+        statusCompleted.click();
+        firstTodo = driver.findElement(By.cssSelector("app-todo-item:nth-child(1) label"));
+        // Only works when the driver is refreshed!!
+        Assertions.assertTrue(firstTodo.isDisplayed());
     }
 
     @Test
@@ -181,8 +198,12 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
+        WebElement tickBox = driver.findElement(By.cssSelector(".toggle"));
+        tickBox.click();
         //assert "clear completed" text is shown in the status bar
+        WebElement clearButton = driver.findElement(By.cssSelector(".clear-completed"));
+        Assertions.assertTrue(clearButton.isDisplayed());
+
     }
 
     @Test
@@ -192,13 +213,14 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
         WebElement tickBox = driver.findElement(By.cssSelector(".toggle"));
         tickBox.click();
         WebElement clearCompleted = driver.findElement(By.cssSelector(".clear-completed"));
         clearCompleted.click();
-        Thread.sleep(2000);
         // assert list is empty
+        WebElement todoList = driver.findElement(By.tagName("app-todo-list"));
+        Assertions.assertEquals("", todoList.getText());
+
     }
 
     @Test
@@ -208,9 +230,7 @@ public class TodoMVC {
         inputBox.click();
         inputBox.sendKeys("buy milk");
         inputBox.sendKeys(Keys.ENTER);
-        Thread.sleep(2000);
         driver.get("https://todomvc.com/examples/vue/dist/#/");
-        Thread.sleep(2000);
         //assert list is empty
     }
 
